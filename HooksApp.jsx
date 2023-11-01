4a94ad1b626c3cef2ad28c9f7cbecab6c160c96c
cristianlana12 <148492938+cristianlana12@users.noreply.github.com>
2023-11-01 12:43:46 -0300

    Add files via upload
    Signed-off-by: cristianlana12 <148492938+cristianlana12@users.noreply.github.com>

---
+++ HooksApp.jsx
@@ -0,0 +1,12 @@
+import { FormulariosComponent } from "./component/formulariosComponent"
+
+
+export const HooksApp = () => {
+  return (
+        <>
+            <h1>Aplicacion de Hooks</h1>
+            <hr />
+            <FormulariosComponent /> 
+        </>
+    )
+}
